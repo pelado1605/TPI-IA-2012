9257a6df8bd3281162c16df95c4fce73281aff38
QuinoNT <QuinoNT@QuinoNT-PC>
2012-09-26 12:18:37 -0300

    Clase Individuo preparada... Creada la clase Operador, falta agregar todo

--- Tpi-IA-2012/src/dominio/Individuo.java
+++ Tpi-IA-2012/src/dominio/Individuo.java
@@ -9,5 +9,212 @@ package dominio;
  * @author Ruben
  */
 public class Individuo {
+
+    private static final byte[][] MMinimos = {
+        {80, 0, 44, 72, 0, 55, 22, 0},
+        {15, 49, 12, 0, 50, 21, 0, 70},
+        {0, 84, 0, 34, 62, 62, 43, 0},
+        {41, 0, 0, 74, 24, 82, 55, 52}
+    };
+    private static final byte[][] MMaximos = {
+        {95, 0, 65, 88, 0, 77, 50, 0},
+        {35, 55, 28, 0, 60, 44, 0, 91},
+        {0, 99, 0, 55, 78, 70, 65, 0},
+        {55, 0, 0, 95, 35, 88, 74, 78}
+    };
+    private static final byte[][] Rangos = {
+        {15, 0, 21, 16, 0, 22, 28, 0},
+        {20, 6, 16, 0, 10, 23, 0, 21},
+        {0, 15, 0, 21, 16, 8, 22, 0},
+        {14, 0, 0, 21, 11, 6, 19, 26}
+    };
+    private static final byte[] Utilidad = {90, 115, 120, 100};
     
+    private float aptitud;
+    private int p1;
+    private int p2;
+    private int p3;
+    private int p4;
+
+    public Individuo(int p1, int p2, int p3, int p4) {
+        this.aptitud = 0f;
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.p4 = p4;
+    }
+
+    public float evaluarAptitud(int[] matIngs) {
+
+        float nuevaAptitud = 0;
+        int[] diferencia = calcDiferencia(matIngs);
+        /*
+         * Aca se va a preguntar por la factibilidad del individuo, es decir, si
+         * los materiales que solicita el individuo estan dentro de los ingre-
+         * -sados por el usario.
+         */
+
+        if (factibilidad(matIngs)) {
+            /*
+             * En es este caso el individuo es factible, hay que ver la utilidad
+             * y los materiales remanentes que deja.
+             */
+
+            /*
+             * Por ser factibles se le suma la utildidad. Ésta no sera lineal,
+             * sino que se elevará al cubo el valor
+             */
+
+            nuevaAptitud += Math.pow(getUtilidad(), 3);
+
+            /*
+             * Aca se trata la puntuacion po utilizacion de los recursos
+             */
+            if (!eficienteConRecursos(matIngs)) {
+                /*
+                 * Si no hay remanente de materiales, se los descontara en
+                 * puntos a la aptitud.
+                 */
+                int diferenciaTotal = 0;
+                for (int i = 0; i < 4; i++) {
+                    if (diferencia[i] < 0) {
+                        diferenciaTotal += Math.pow(diferencia[i], 2);
+                    }
+                }
+                nuevaAptitud -= diferenciaTotal;
+            } else {
+                /*
+                 * El individuo que utilice los materiales de manera más
+                 * eficiente (que el remanente de materiales sea 0), obtendra un
+                 * premio en puntos de aptitud
+                 */
+                nuevaAptitud += (nuevaAptitud * 0.50);
+            }
+
+        } else {
+            /*
+             * Aca, se castiga severamente al individuo con una aptitud muuuuy
+             * baja, debido a que no es factible porque pide mas materiales que
+             * los ingresados/existentes.
+             */
+            int diferenciaTotal = 0;
+            for (int i = 0; i < 8; i++) {
+                if (diferencia[i] < 0) {
+                    diferenciaTotal += Math.pow(diferencia[i], 2);
+                }
+            }
+            nuevaAptitud = nuevaAptitud - diferenciaTotal;
+        }
+
+        this.aptitud = nuevaAptitud;
+        return nuevaAptitud;
+    }
+
+    public Individuo cruzarse(Individuo unIndividuo) {
+        return null;
+    }
+
+    public void mutarse() {
+    }
+
+    public boolean eficienteConRecursos(int[] matIngs) {
+        /*
+         * Devuelve verdadero si (diferencia - rango*cantDeProductos)<=0 Si es
+         * es menor pone 0 en la diferencia. Los valores de diferencia que
+         * queden positivos, seran el remanente del material.
+         */
+        int[] diferencia = calcDiferencia(matIngs);
+        boolean valor = false;
+        int[] rangos = calcularMaterialesRango();
+
+        for (int i = 0; i < rangos.length; i++) {
+            diferencia[i] -= rangos[i];
+            if ((diferencia[i]) <= 0) {
+                valor = true;
+                diferencia[i] = 0;
+            }
+        }
+
+        return valor;
+    }
+
+    public int[] calcularMaterialesMinimos() {
+        int[] materiales = new int[8];
+        for (int i = 0; i < materiales.length; i++) {
+            materiales[i] = p1 * MMinimos[0][i] + p2 * MMinimos[1][i]
+                    + p3 * MMinimos[2][i] + p4 * MMinimos[3][i];
+        }
+        return materiales;
+    }
+
+    public int[] calcularMaterialesRango() {
+        int[] materiales = new int[8];
+        for (int i = 0; i < materiales.length; i++) {
+            materiales[i] = p1 * Rangos[0][i] + p2 * Rangos[1][i]
+                    + p3 * Rangos[2][i] + p4 * Rangos[3][i];
+        }
+        return materiales;
+    }
+
+    public boolean factibilidad(int[] matIngs) {
+
+        int[] diferencia = calcDiferencia(matIngs);
+        boolean valor = true;
+        for (int i = 0; i < diferencia.length; i++) {
+            if (diferencia[i] < 0) {
+                valor = false;
+            }
+        }
+        return valor;
+    }
+
+    private int[] calcDiferencia(int[] matIngs) {
+        int[] matMinimos = calcularMaterialesMinimos();
+        int[] diferencia = new int[8];
+        for (int i = 0; i < diferencia.length; i++) {
+            diferencia[i] = matIngs[i] - matMinimos[i];
+        }
+        return diferencia;
+    }
+
+    public int getUtilidad() {
+
+        return 0;
+    }
+
+    public float getAptitud() {
+        return aptitud;
+    }
+
+    public int getP1() {
+        return p1;
+    }
+
+    public int getP2() {
+        return p2;
+    }
+
+    public int getP3() {
+        return p3;
+    }
+
+    public int getP4() {
+        return p4;
+    }
+
+    public void setP1(int p1) {
+        this.p1 = p1;
+    }
+
+    public void setP2(int p2) {
+        this.p2 = p2;
+    }
+
+    public void setP3(int p3) {
+        this.p3 = p3;
+    }
+
+    public void setP4(int p4) {
+        this.p4 = p4;
+    }
 }
---
+++ Tpi-IA-2012/src/dominio/Operador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+/**
+ *
+ * @author QuinoNT
+ */
+public class Operador {
+    
+    public final static byte CRUZA_SIMPLE = 0;
+    public final static byte CRUZA_BINOMIAL = 1;
+    public final static byte CRUZA_MULTIPUNTO = 2;    
+    
+
+}
