9192499c3e85bcfb87eaef5ac4893c56bb91a6b5
Ruben <Ruben@Vostro-NB>
2012-10-01 10:59:05 -0300

    agregue unos atributos y constructores nomas en poblacion. Hay que seguir ahi

--- Tpi-IA-2012/src/dominio/Poblacion.java
+++ Tpi-IA-2012/src/dominio/Poblacion.java
@@ -4,10 +4,34 @@
  */
 package dominio;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author Ruben
  */
 public class Poblacion {
+
+    private ArrayList<Individuo> poblado;
+    private Random suerte = new Random();
+    private float aptitud = 0f;
+    private float probMutacion;
+
+    public Poblacion(float probMutacion) {
+
+        this.probMutacion = probMutacion;
+    }
+
+    /**
+     * Constructor para la creación de la población inicial.
+     * @param poblado 
+     */
+    public Poblacion(ArrayList<Individuo> poblado) {
+        this.poblado = poblado;
+    }
+    
+    
+    
     
 }
